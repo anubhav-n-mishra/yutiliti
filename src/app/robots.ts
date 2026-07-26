cca676fcf149c50e482fe992555631e693853b79
TONYSTARKOP <msonu2018@gmail.com>
2026-07-26 11:36:23 +0530

    sitemap robots.txt on page seo optimization and fixes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,31 +3,14 @@ import { SITE_URL } from "@/src/lib/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/api/", "/_next/", "/static/", "/private/"],
-      },
-      {
-        userAgent: "Googlebot",
-        allow: "/",
-        disallow: ["/api/", "/_next/", "/static/", "/private/"],
-      },
-      {
-        userAgent: "Bingbot",
-        allow: "/",
-        disallow: ["/api/", "/_next/", "/static/", "/private/"],
-      },
-      {
-        userAgent: "Googlebot-Image",
-        allow: "/",
-      },
-      {
-        userAgent: "Googlebot-Video",
-        allow: "/",
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/api/",
+        "/private/",
+      ],
+    },
     sitemap: `${SITE_URL}/sitemap.xml`,
     host: SITE_URL,
   };
